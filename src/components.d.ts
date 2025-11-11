63b7775864e3328e845c0fccc32063e09c384581
Simon Mika <simon@mika.se>
2025-11-11 19:06:26 +0100

    Updated config works.

--- src/components.d.ts
+++ src/components.d.ts
@@ -6,6 +6,7 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 import { Bounds, Hero, Map, Point } from "./model";
+export { Bounds, Hero, Map, Point } from "./model";
 export namespace Components {
     interface AronGame {
     }
